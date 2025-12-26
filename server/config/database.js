2c5647aa95f24bcedf61bbf08137ad712ddbacf8
PriYad93 <pritiyadav880272@gmail.com>
2025-12-26 23:22:28 +0530

    Fix env, MongoDB config and replace bcrypt with bcryptjs

--- server/config/database.js
+++ server/config/database.js
@@ -2,13 +2,14 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect = () => {
-    mongoose.connect(process.env.MONGO_URI, {
-       dbName : "Edtech"
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "Edtech",
     })
     .then(() => console.log("DB Connected Successfully"))
-    .catch( (error) => {
-        console.log("DB Connection Failed");
-        console.error(error);
-        process.exit(1);
-    } )
+    .catch((error) => {
+      console.log("DB Connection Failed");
+      console.error(error);
+      process.exit(1);
+    });
 };
